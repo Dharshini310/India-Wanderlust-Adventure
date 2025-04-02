b764fa6feef137095036aef12772b87f5e0b14f5
GUDDATI DHARSHINI <133137635+Dharshini310@users.noreply.github.com>
2025-04-02 10:15:35 +0530

    Update Animatedvideo.js

--- src/Components/Animatedvideo.js
+++ src/Components/Animatedvideo.js
@@ -6,7 +6,7 @@ const AnimatedVideo = () => {
     <div className="animated-video">
       {/* Render your images here */}
       <img src="https://studywrap.com/wp-content/uploads/2022/08/GK-List-of-Famous-Waterfalls-in-India.jpg" alt="loading.." />
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6j4WPN0tJiQ3t5yE7RRXPhRThfkwIi7J0zQ&usqp=CAU" alt="loading.." />
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2Qd8849_igWknovTPLLyAlXQW6sjXeFQhKA&s" alt="loading.." />
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTeGo0s8bcUCGjS0fezxEE16XD0EjznqugXg&usqp=CAU" alt="loading.."/>
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqXt-UPfR-aNu5MyDsHp9jZvh9ksghg24zzw&usqp=CAU" alt="loading.."/>
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyRQg16aEA45ohXWw69lUba8a_QLdX3I2b5A&usqp=CAU" alt="loading.."/>
